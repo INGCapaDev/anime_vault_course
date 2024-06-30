ac68e387d014234dfdb84502643da62366708ac3
INGCapaDev <ingcapadev@gmail.com>
2024-06-29 20:26:19 -0700

    :sparkles: [FEAT]: Add infinite scroll feature

---
+++ app/action.ts
@@ -0,0 +1,37 @@
+'use server';
+
+export type AnimeData = {
+  mal_id: number;
+  images: {
+    webp: {
+      image_url: string;
+    };
+  };
+  title: string;
+  score: number;
+  rank: number;
+  type: string;
+  episodes: number;
+};
+
+const BASE_URL = 'https://api.jikan.moe/v4/anime' as const;
+
+export const fetchAnime = async (page: number) => {
+  const response = await fetch(
+    BASE_URL + `?page=${page}&limit=8&order_by=popularity`
+  );
+  const { data, pagination } = await response.json();
+  console.log(pagination);
+
+  return {
+    data,
+    pagination,
+  } as {
+    data: AnimeData[];
+    pagination: {
+      last_visible_page: number;
+      has_next_page: boolean;
+      current_page: number;
+    };
+  };
+};
--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,18 +1,51 @@
-import Image from "next/image";
+'use client';
+import { AnimeData, fetchAnime } from '@/app/action';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
+import AnimeCard from './AnimeCard';
 
+let page = 2;
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeData[]>([]);
+  const [isFinished, setIsFinished] = useState(false);
+
+  useEffect(() => {
+    if (inView && !isFinished) {
+      fetchAnime(page).then((res) => {
+        setData((prev) => [...prev, ...res.data]);
+        page++;
+        if (res.pagination.last_visible_page === page - 1) {
+          setIsFinished(true);
+        }
+      });
+    }
+  }, [inView, isFinished]);
+
   return (
     <>
-      <section className="flex justify-center items-center w-full">
-        <div>
-          <Image
-            src="./spinner.svg"
-            alt="spinner"
-            width={56}
-            height={56}
-            className="object-contain"
-          />
-        </div>
+      <section className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10'>
+        {data.map((item: AnimeData, index: number) => (
+          <AnimeCard key={item.mal_id} anime={item} index={index} />
+        ))}
+      </section>
+      <section className='flex justify-center items-center w-full'>
+        {isFinished ? (
+          <p className='text-white text-lg font-semibold'>
+            You have reached the end!
+          </p>
+        ) : (
+          <div ref={ref}>
+            <Image
+              src='./spinner.svg'
+              alt='spinner'
+              width={56}
+              height={56}
+              className='object-contain'
+            />
+          </div>
+        )}
       </section>
     </>
   );
